db21dd27274c5eb69b96489afdf907e3983df82a
OLADISEA <ladisea55@gmail.com>
2024-06-01 23:00:04 +0100

    Added the necessary dependencies and the app images

---
+++ lib/src/ui/common/app_colors/app_colors.dart
@@ -0,0 +1,9 @@
+import 'dart:ui';
+
+class AppColor{
+  static const splashScreenColor = Color(0XFF8E6CEF);
+  static const whiteColor = Color(0XFFFFFFFF);
+  static const blackColor = Color(0XFF272727);
+  static const blackShade = Color(0XFF272727);
+  static const darkGrey = Color(0XFFF4F4F4);
+}

0d958c714d8f590d562fd165d3b9f33c8934d4aa
OLADISEA <ladisea55@gmail.com>
2024-06-02 11:29:53 +0100

    Added the Multi bloc provider for state management and also implemented the Themedata which will include the light and dark mode

---
+++ lib/main.dart
@@ -0,0 +1,46 @@
+import 'package:fashion_finds/src/bloc/theme_bloc/theme_bloc.dart';
+import 'package:fashion_finds/src/bloc/theme_bloc/theme_mode.dart';
+import 'package:fashion_finds/src/bloc/theme_bloc/theme_state.dart';
+import 'package:fashion_finds/src/ui/common/theme/theme_constants.dart';
+import 'package:fashion_finds/src/ui/screen/splash_screen/splash_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+void main() {
+  runApp(
+    MultiBlocProvider(
+          providers: [
+            BlocProvider<ThemeBloc>(
+              create: (context) => ThemeBloc(),
+            ),
+          ],
+      child: const MyApp(),
+    )
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return ScreenUtilInit(
+      designSize: const Size(390,844),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      child: BlocBuilder<ThemeBloc, ThemeState>(
+        builder: (context, themeState) {
+          ThemeData themeData = themeState.status == AppThemeMode.light ? ThemeConstants.lightMode : ThemeConstants.darkMode;
+          return MaterialApp(
+            debugShowCheckedModeBanner: false,
+            theme: themeData,
+            home: const SplashScreen(),
+          );
+        },
+      ),
+    );
+  }
+}
+

f58c8a8078eb4ef71f68d67fbc1e0c4854acc569
OLADISEA <ladisea55@gmail.com>
2024-06-03 21:02:31 +0100

    Implemented the home page showcasing different tabs such as the fashion tab, notification tab, order, and settings tab

---
+++ lib/src/ui/screen/home_page/home_page.dart
@@ -0,0 +1,110 @@
+
+import 'package:fashion_finds/src/ui/screen/fashion_page/fashion_page.dart';
+import 'package:fashion_finds/src/ui/screen/notifications_page/notifications_page.dart';
+import 'package:fashion_finds/src/ui/screen/orders_page/order_page.dart';
+import 'package:flutter/material.dart';
+import 'package:persistent_bottom_nav_bar/persistent_tab_view.dart';
+
+import '../../common/app_colors/app_colors.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  late PersistentTabController _controller;
+  late bool _hideNavBar;
+
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = PersistentTabController(initialIndex: 0);
+    _hideNavBar = false;
+  }
+
+
+
+
+
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Container(),
+        ],
+      ),
+      bottomNavigationBar: PersistentTabView(
+        context,
+        controller: _controller,
+        screens: [
+          FashionPage(),
+          //Container(color: Colors.red), // Home Screen
+          NotificationsPage(),
+          OrderPage(),
+          Container(child: Center(child: Text("Settings Page"),),), // Settings Screen
+        ],
+        items: [
+          PersistentBottomNavBarItem(
+              icon: const Icon(Icons.home),
+              //title: 'Home',
+              activeColorPrimary: AppColor.splashScreenColor,
+              inactiveColorPrimary: AppColor.blackColor
+          ),
+          PersistentBottomNavBarItem(
+              icon: const Icon(Icons.event_note_sharp),
+              //title: 'Details',
+              activeColorPrimary: AppColor.splashScreenColor,
+              inactiveColorPrimary: AppColor.blackShade
+          ),
+          PersistentBottomNavBarItem(
+              icon: const Icon(Icons.video_library),
+              //title: 'Videos',
+              activeColorPrimary: AppColor.splashScreenColor,
+              inactiveColorPrimary: AppColor.blackShade
+          ),
+          PersistentBottomNavBarItem(
+              icon: const Icon(Icons.settings),
+              //title: 'Settings',
+              activeColorPrimary:AppColor.splashScreenColor,
+              inactiveColorPrimary: AppColor.blackColor
+          ),
+        ],
+        confineInSafeArea: true,
+        backgroundColor: Colors.white,
+        handleAndroidBackButtonPress: true,
+        resizeToAvoidBottomInset: true,
+        stateManagement: true,
+        hideNavigationBar: _hideNavBar, // Pass the _hideNavBar value here
+        hideNavigationBarWhenKeyboardShows: true,
+        margin: const EdgeInsets.all(0.0),
+        popActionScreens: PopActionScreensType.all,
+        bottomScreenMargin: 0.0,
+        decoration: NavBarDecoration(
+          borderRadius: BorderRadius.circular(10.0),
+          colorBehindNavBar: Colors.white,
+        ),
+        popAllScreensOnTapOfSelectedTab: true,
+        itemAnimationProperties: ItemAnimationProperties(
+          duration: Duration(milliseconds: 200),
+          curve: Curves.ease,
+        ),
+        screenTransitionAnimation: ScreenTransitionAnimation(
+          animateTabTransition: true,
+          curve: Curves.ease,
+          duration: Duration(milliseconds: 200),
+        ),
+        navBarStyle: NavBarStyle.style3,
+      ),
+    );
+  }
+}

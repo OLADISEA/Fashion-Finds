8f71af81fb0ba2a379855febfeef6ec09f61633e
OLADISEA <ladisea55@gmail.com>
2024-06-06 14:39:04 +0100

    Created the top selling model for efficient api request.

---
+++ lib/src/models/top_selling_model.dart
@@ -0,0 +1,8 @@
+class TopSellingModel{
+  final String text;
+  bool isFav;
+  final String price;
+  final String image;
+
+  TopSellingModel({required this.text, this.isFav = false, required this.price, required this.image});
+}

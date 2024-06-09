effa3e4c371843d3e289b08ccafba8f1d504a10c
OLADISEA <ladisea55@gmail.com>
2024-06-09 12:48:34 +0100

    Refactored some codes

--- lib/src/ui/screen/settings_page/widget/widget.dart
+++ lib/src/ui/screen/settings_page/widget/widget.dart
@@ -1,21 +1,25 @@
 import 'package:fashion_finds/src/ui/common/app_colors/app_colors.dart';
 import 'package:fashion_finds/src/ui/common/widgets/widgets.dart';
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-Widget userInfo(){
+Widget userInfo(BuildContext context){
   return Container(
+    decoration: BoxDecoration(
+      color: Theme.of(context).colorScheme.tertiary,
+
+    ),
     margin: EdgeInsets.symmetric(horizontal: 24.w),
     padding: EdgeInsets.symmetric(horizontal: 16.w,vertical: 13.h),
     child: Row(
-      mainAxisAlignment: MainAxisAlignment.center,
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
       children: [
         Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             reusableText(text: "Gilbert Jones"),
-            reusableText(text: "Gilbertjones001@gmail.com"),
-            reusableText(text: "121-2247890")
+            reusableText(text: "Gilbertjones001@gmail.com",color: Theme.of(context).colorScheme.onPrimary),
+            reusableText(text: "121-224-7890")
           ],
         ),
         myTextButton(text: "Edit", onTap: (){},color: AppColor.splashScreenColor)

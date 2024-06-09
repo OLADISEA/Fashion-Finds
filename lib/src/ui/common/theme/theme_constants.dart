07f51674ca7285286602ce3480fdcb2ba642e79b
OLADISEA <ladisea55@gmail.com>
2024-06-09 12:48:14 +0100

    Made some adjustment to the color scheme

--- lib/src/ui/common/theme/theme_constants.dart
+++ lib/src/ui/common/theme/theme_constants.dart
@@ -1,21 +1,27 @@
+import 'package:fashion_finds/src/ui/common/app_colors/app_colors.dart';
 import 'package:flutter/material.dart';
 
 class ThemeConstants {
   static ThemeData lightMode = ThemeData(
     colorScheme: ColorScheme.light(
-      background: Colors.grey.shade300,
+      background: AppColor.whiteColor,
       primary: Colors.grey.shade500,
       secondary: Colors.grey.shade200,
-      inversePrimary: Colors.grey.shade900,
+      inversePrimary: Color(0XFF342F3F),
+      tertiary: AppColor.darkGrey,
+      onPrimary: AppColor.blackColor
     ),
   );
 
   static ThemeData darkMode = ThemeData(
     colorScheme: ColorScheme.dark(
-      background: Colors.grey.shade900,
+      background: Color(0XFF1D182A),
       primary: Colors.grey.shade600,
       secondary: Colors.grey.shade800,
-      inversePrimary: Colors.grey.shade300,
+      inversePrimary: Color(0XFF342F3F),
+        tertiary: Color(0XFF342F3F),
+      onPrimary: AppColor.whiteColor
+
     ),
   );
 }
